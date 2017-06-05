24a57a3cecd2e884c341e0600cd165e7865d9ed0
TryIO <io.trydent@gmail.com>
2017-06-05 20:13:53 +0200

    adding authorization

--- youth-webapp/src/main/kotlin/io/youth/Main.kt
+++ youth-webapp/src/main/kotlin/io/youth/Main.kt
@@ -1,5 +1,6 @@
 package io.youth
 
+import com.typesafe.config.Config
 import io.youth.service.person.AddPerson
 import io.youth.service.person.EditPerson
 import io.youth.service.person.PersonModule
@@ -7,21 +8,61 @@ import io.youth.service.person.RemovePerson
 import io.youth.service.person.personCommand
 import io.youth.service.person.personQuery
 import io.youth.service.person.profilePersons
+import org.jooby.Err
 import org.jooby.MediaType.json
 import org.jooby.Request
 import org.jooby.Results
+import org.jooby.Route
+import org.jooby.Status
 import org.jooby.Status.NOT_FOUND
 import org.jooby.Status.UNPROCESSABLE_ENTITY
 import org.jooby.flyway.Flywaydb
 import org.jooby.handlers.CorsHandler
 import org.jooby.json.Gzon
+import org.jooby.pac4j.Auth
+import org.jooby.pac4j.AuthStore
+import org.jooby.require
 import org.jooby.run
 import org.jooby.rx.Rx
 import org.jooby.rx.RxJdbc
+import org.pac4j.core.profile.CommonProfile
+import org.pac4j.http.client.direct.ParameterClient
+import org.pac4j.jwt.config.signature.SecretSignatureConfiguration
+import org.pac4j.jwt.config.signature.SignatureConfiguration
+import org.pac4j.jwt.credentials.authenticator.JwtAuthenticator
+import org.pac4j.jwt.profile.JwtGenerator
+import org.pac4j.oauth.client.FacebookClient
+import org.pac4j.oidc.client.OidcClient
+import org.pac4j.oidc.config.OidcConfiguration
+import org.pac4j.oidc.profile.OidcProfile
+import java.util.Objects.isNull
+
 
 private fun Request.uuid() = this.param("uuid").value()
 private fun Request.addPerson() = this.body().to(AddPerson::class.java)
 private fun Request.editPerson() = this.body().to(EditPerson::class.java)
+
+val getUserProfile: (req: Request) -> CommonProfile = {
+  val id = it.ifGet<String>(Auth.ID).orElseGet { it.session().get(Auth.ID).value(null) }
+  if (isNull(id)) throw Err(Status.UNAUTHORIZED)
+  val store = it.require(AuthStore::class)
+
+  store.get(id).get()
+}
+
+data class OpenIDProfile(val client: String, val profile: CommonProfile)
+data class Token(val token: String)
+
+val handler = Route.OneArgHandler {
+  val profile = getUserProfile(it)
+
+  OpenIDProfile(
+    client = profile::class.java.simpleName.replace("Profile", ""),
+    profile = profile
+  )
+}
+
+class GoogleClient(conf: OidcConfiguration): OidcClient<OidcProfile>(conf)
 
 fun main(args: Array<String>) {
   run(*args) {
@@ -32,8 +73,58 @@ fun main(args: Array<String>) {
     use(RxJdbc())
     use(PersonModule())
 
+    get("*") { req, _ -> req.session().get(Auth.ID).toOptional().ifPresent { req.set("logged", it) } }
+
+    get("/token") { req ->
+      val profile = getUserProfile(req)
+      val config = req.require(Config::class.java)
+      val jwtGenerator = JwtGenerator<CommonProfile>(config.getString("jwt.salt"))
+      val token = jwtGenerator.generate(profile)
+
+      Token(token)
+    }
+
+    use(Auth()
+      .client("/facebook/**") {
+        FacebookClient(it.getString("fb.key"), it.getString("fb.secret"))
+      }
+
+      .client("/google/**") {
+        OidcConfiguration().apply {
+          clientId = it.getString("oidc.clientID")
+          secret = it.getString("oidc.secret")
+          discoveryURI = it.getString("oidc.discoveryURI")
+          addCustomParam("prompt", "consent")
+        }.let {
+          GoogleClient(it)
+        }
+      }
+      .client("/jwt/*") { conf ->
+        SecretSignatureConfiguration(conf.getString("jwt.salt"))
+          .let { arrayOf<SignatureConfiguration>(it).toMutableList() }
+          .let { JwtAuthenticator(it) }
+          .let {
+            ParameterClient("token", it).apply {
+              isSupportGetRequest = true
+              isSupportPostRequest = false
+            }
+          }
+      }
+    )
+
+    get("/profile", handler)
+
+    get("/google", handler)
+
+    get("/facebook", handler)
+
+    get("/jwt", handler)
+
+    get("/token", handler)
+
+
     use("/")
-      .get { -> "You should look at the Person API on /person."}
+      .get { -> "You should look at the Person API on /person." }
       .consumes(json)
       .produces(json)
 
